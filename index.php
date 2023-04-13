fd775349fdad5590882fef756cc0adfed82df135
vanyarajwadevy24 <vanyarajwadevy@gmail.com>
2023-04-13 22:38:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<center>";
+    echo "<img src='profile.jpg' width=113.4px height=151,2px>";
+    echo "<p> Nama : Vanya Rajwadevy </p>";
+    echo "<p> Tempat Tanggal Lahir : Jakarta, 24 Agustus 2002 </p>";
+    echo "<p> Alamat : Jakarta Timur - Dki Jakarta </p>";
+    echo "<p> Riwayat Pendidikan : </p>";
+    echo "<p> ( SD 11 Pagi 2008 - 2014 ) </p>";
+    echo "<p> ( SMPN 272 Jakarta 2014 - 2017 ) </p>";
+    echo "<p> ( SMKN 24 Jakarta 2017 - 2020 ) </p>";
+    echo "<p> ( Universitas Persada Indonesia YAI  2020 - Sekarang ) </p>";
+    echo "<p> Hobby : Nonton Film </p>";
+?>
+
